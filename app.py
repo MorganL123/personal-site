39ce4e5daac14490384c0a705ac6f986d7176500
MorganL123 <morganleelin@gmail.com>
2024-09-01 21:20:40 -0400

    sijdof

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/genre', methods=['POST'])
+def genre_fn():
+    genre = request.form['dropdown']
+    response = requests.get(f"https://openlibrary.org/subjects/{genre}.json") 
+    data = response.json()  
+    #return data['works'][0]['title']
+    return render_template('genre.html',data=data)
+
 if __name__== '__main__':
     app.run(debug=True, host ='0.0.0.0', port = 9000)
