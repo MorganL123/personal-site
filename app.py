ee17c7044e8d6390d3bc1b6e5b61edba2d64e85d
MorganL123 <100043415+MorganL123@users.noreply.github.com>
2024-09-05 02:44:07 -0400

    im so tired

--- app.py
+++ app.py
@@ -7,5 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/photos')
+def photos():
+    print("Photos page accessed")
+    return render_template('photos.html')
+
+
+@app.route('/floral')
+def floral():
+    print("Florals page accessed")
+    return render_template('floral.html')
+
 if __name__== '__main__':
     app.run(debug=True, host ='0.0.0.0', port = 9000)
